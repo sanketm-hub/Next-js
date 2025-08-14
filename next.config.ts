7b05bb2775a8470021f0de530f6b09126bdc2acb
sanketm-hub <sanketm@salttechno.com>
2025-08-14 16:57:58 +0530

    config file updated

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
 };
 
 
 
 export default nextConfig;
+
+
+
+
