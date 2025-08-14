acc3d9fff111d0b08b18256be41e8fbce3b8d7f7
sanketm-hub <sanketm@salttechno.com>
2025-08-14 18:15:27 +0530

    home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { use, useState } from 'react';
+import { useState } from 'react';
 
 import Image from "next/image";
 import Safetyimg1 from "./media/Layer_1.svg";
